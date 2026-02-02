7da05c1cf3d04578ac97929baf353c2cad35417b
Amith Ganesh <amith.ganesh1284@gmail.com>
2026-02-02 10:44:51 -0600

    Using actuator to ensure dynamic config changes

--- cards/src/main/java/com/microservices/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/microservices/cards/dto/CardsContactInfoDto.java
@@ -1,10 +1,18 @@
 package com.microservices.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contact, List<String> support) {
+@Getter
+@Setter
+public class CardsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contact;
+    private List<String> support;
 }
